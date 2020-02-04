229a1e2bac7e061ad3070f81e10238abc965a2aa
Jason Kurtz <JasonKurtzWebDeveloper@gmail.com>
2020-02-04 16:24:00 -0500

    Declared variable in test.js and used while loop to console.log 5 times

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-// declares a variable
-var myAge = 26;
+var age = 5;
 
-// evaluates variable
-if (myAge < 18 || myAge > 30 || myAge === 25) {
-	// writes if variable meets condition
-	document.write("You ain't coming!");
-} else {
-	// writes for all other conditions
-	document.write("You can come, you cool dude.");
+while (age < 10) {
+	console.log("Your age is less than 10");
+	age++;
 }
+
+document.write("You are now over 10");
