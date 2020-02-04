4137501108b049ca26467fabaa5689753ba0a41a
Jason Kurtz <JasonKurtzWebDeveloper@gmail.com>
2020-02-04 14:25:52 -0500

    Declared variable and added if/else statement to test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-alert("Hello, World!");
+// declares variable
+var youLikeMeat = true;
+
+// evalutates whether variable is true or false
+if (youLikeMeat) {
+	// if true
+	document.write("you like meat");
+} else {
+	// if false
+	document.write("you hate meat");
+}
