492dd64ff8110bf0fb1b1544d163251524ea85c9
Jason Kurtz <JasonKurtzWebDeveloper@gmail.com>
2020-02-04 16:31:42 -0500

    Added comments to test.js file

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
+// declared variable
 var age = 5;
 
+// loop through based on age variable
 while (age < 10) {
+	// log based on age variable being under 10 and then incrementing by 1
 	console.log("Your age is less than 10");
 	age++;
 }
 
+// write when loop is finished executing
 document.write("You are now over 10");
